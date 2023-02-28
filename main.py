fb7bbc0b46509f7b982f93b42b6af5a504203a87
brunoguimarao <b@atvy.us>
2023-02-27 21:16:30 -0300

    Verificação em vários IPs
    Agora o script pode verificar a OID de contador de impressão em vários IPS de uma só vez.

--- main.py
+++ main.py
@@ -1,24 +1,26 @@
 import os
 from pysnmp.hlapi import *
 
-# Define o endereço IP da impressora e as credenciais SNMP
-ip = '10.44.74.37'
+# Define uma lista de endereços IP da impressora e as credenciais SNMP
+ips = ['10.44.74.37', '10.44.74.28']
 comunidade = 'public'
 
 # Define os OIDs para os contadores de páginas monocromáticas e coloridas
 oid_monocromatica = '1.3.6.1.2.1.43.10.2.1.4.1.1'
 
-# Cria uma sessão SNMP com a impressora para o contador de páginas monocromáticas
-iterator = getCmd(SnmpEngine(),
-                  CommunityData(comunidade),
-                  UdpTransportTarget((ip, 161)),
-                  ContextData(),
-                  ObjectType(ObjectIdentity(oid_monocromatica)))
+# Loop através dos endereços IP e obter o número de páginas impressas de cada impressora
+for ip in ips:
+    # Cria uma sessão SNMP com a impressora para o contador de páginas monocromáticas
+    iterator = getCmd(SnmpEngine(),
+                      CommunityData(comunidade),
+                      UdpTransportTarget((ip, 161)),
+                      ContextData(),
+                      ObjectType(ObjectIdentity(oid_monocromatica)))
 
-# Obtém o valor do contador de páginas monocromáticas
-errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
-if errorIndication:
-    print(errorIndication)
-else:
-    for varBind in varBinds:
-        print('Número de páginas monocromáticas impressas:', varBind[1])
+    # Obtém o valor do contador de páginas monocromáticas
+    errorIndication, errorStatus, errorIndex, varBinds = next(iterator)
+    if errorIndication:
+        print('Erro:', errorIndication)
+    else:
+        for varBind in varBinds:
+            print(f'Impressora {ip}: Número de páginas monocromáticas impressas:', varBind[1])
